bf92ffa15900809b7ea13da5a97baae2ee14f6d2
Pankaj Sharma <pankaj.sharma@omniware.in>
2025-03-12 09:46:40 +0530

    cleanup

--- Backend/services/captain.services.js
+++ Backend/services/captain.services.js
@@ -4,13 +4,6 @@ module.exports.createCaptain = async ({
     firstname, lastname, email, password,
     color, plate, capacity, vehicleType
 }) => {
-    console.log("firstname => ", firstname,
-        "email => ", email,
-        "password => ", password,
-        "color => ", color,
-        "plate => ", plate,
-        "capacity => ", capacity,
-        "vehicleType => ", vehicleType,)
     if (!firstname || !email || !password || !color || !plate || !capacity || !vehicleType) {
         throw new Error('All fields are required');
     }
